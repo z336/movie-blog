ebdc943b7ce56897793ddd88a033fa4efe32f91e
jon <jcoleman1388@gmail.com>
2019-04-20 20:18:10 -0400

    404 page

---
+++ src/pages/404.js
@@ -0,0 +1,24 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import Head from "../components/head"
+import styles from "./404.module.scss"
+
+const ErrorPage = () => {
+  return (
+    <div>
+      <Layout>
+        <Head title="404" />
+        <h1>That page doesn't exist</h1>
+        <p>
+          <Link to="/" className={styles.link}>
+            Home
+          </Link>
+        </p>
+      </Layout>
+    </div>
+  )
+}
+
+export default ErrorPage
