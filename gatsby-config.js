e394db3ed97759ec07176f67686f4303b5bca186
jon <jcoleman1388@gmail.com>
2019-04-21 00:56:32 -0400

    markdown external links

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,6 +28,13 @@ module.exports = {
               linkImagesToOriginal: false,
             },
           },
+          {
+            resolve: "gatsby-remark-external-links",
+            options: {
+              target: "_blank",
+              rel: "nofollow",
+            },
+          },
         ],
       },
     },
